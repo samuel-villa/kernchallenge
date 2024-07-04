4c91543bd963c3bac3f36955291b672298b5e1b8
samuel-villa <ciullasamuel@gmail.com>
2024-07-04 23:46:39 +0200

    [IMP] allowing to perform checkin and checkout action from the index template

--- kern_timesheet_app/timesheet.py
+++ kern_timesheet_app/timesheet.py
@@ -11,27 +11,49 @@ import datetime
 bp = Blueprint('timesheet', __name__)
 
 
-@bp.route('/')
-def index():
+INDEX = 'timesheet/index.html'
+INDEX_ROUTE = 'timesheet.index'
+CREATE = 'timesheet/create.html'
+
+
+def get_timesheets():
     db = get_db()
     timesheets = db.execute(
         'SELECT t.id, checkin, checkout, user_id'
         ' FROM timesheet t JOIN user u ON t.user_id = u.id'
-        ' ORDER BY checkin DESC'
+        ' ORDER BY t.id DESC'
     ).fetchall()
-    return render_template('timesheet/index.html', timesheets=timesheets)
+    return timesheets
+
+
+@bp.route('/')
+def index():
+    timesheets = get_timesheets()
+    return render_template(INDEX, timesheets=timesheets)
 
 
 @bp.route('/create', methods=('GET', 'POST'))
 @login_required
 def create():
     if request.method == 'POST':
+        db = get_db()
+        db.execute(
+            'INSERT INTO timesheet (user_id)'
+            ' VALUES (?)',
+            (g.user['id'],)
+        )
+        db.commit()
+        return redirect(url_for(INDEX_ROUTE))
 
-        # TODO checkout logic
+    return render_template(INDEX)
+
+
+@bp.route('/<int:id>/checkin', methods=('GET', 'POST'))
+@login_required
+def checkin(id):
+    if request.method == 'POST':
         timecheck = datetime.datetime.now()
-        # checkin = timecheck.strftime("%a %d %b %Y %H:%M:%S")
         checkin = timecheck
-        # checkout = None
         error = None
 
         if not checkin:
@@ -42,14 +64,43 @@ def create():
         else:
             db = get_db()
             db.execute(
-                'INSERT INTO timesheet (checkin, user_id)'
-                ' VALUES (?, ?)',
-                (checkin, g.user['id'])
+                'UPDATE timesheet SET checkin = ?, user_id = ?'
+                ' WHERE id = ?',
+                (checkin, g.user['id'], id)
             )
             db.commit()
-            return redirect(url_for('timesheet.index'))
+            timesheets = get_timesheets()
+            return render_template(INDEX, timesheets=timesheets)
 
-    return render_template('timesheet/create.html')
+    return render_template(INDEX)
+
+
+@bp.route('/<int:id>/checkout', methods=('GET', 'POST'))
+@login_required
+def checkout(id):
+    if request.method == 'POST':
+
+        timecheck = datetime.datetime.now()
+        checkout = timecheck
+        error = None
+
+        if not checkout:
+            error = 'Checkout is required.'
+
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE timesheet SET checkout = ?, user_id = ?'
+                ' WHERE id = ?',
+                (checkout, g.user['id'], id)
+            )
+            db.commit()
+            timesheets = get_timesheets()
+            return render_template(INDEX, timesheets=timesheets)
+
+    return render_template(INDEX)
 
 
 def get_timesheet(id, check_user=True):
@@ -92,7 +143,7 @@ def update(id):
                 (checkin, checkout, id)
             )
             db.commit()
-            return redirect(url_for('timesheet.index'))
+            return redirect(url_for(INDEX_ROUTE))
 
     return render_template('timesheet/update.html', timesheet=timesheet)
 
@@ -104,4 +155,4 @@ def delete(id):
     db = get_db()
     db.execute('DELETE FROM timesheet WHERE id = ?', (id,))
     db.commit()
-    return redirect(url_for('timesheet.index'))
+    return redirect(url_for(INDEX_ROUTE))
